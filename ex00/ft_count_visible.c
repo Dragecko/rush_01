b77657cc3d269daec58a9b4050f5409e2fe9cbc1
Esteban Lhirondelle <elhirond@c1r13s2.42lausanne.ch>
2025-07-05 18:15:04 +0200

    add ft_count_visible.

---
+++ ex00/ft_count_visible.c
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                         ::::::::           */
+/*   ft_count_visible.c                                  :+:    :+:           */
+/*                                                      +:+                   */
+/*   By: elhirond <marvin@42.fr>                       +#+                    */
+/*                                                    +#+                     */
+/*   Created: 2025/07/05 17:56:30 by elhirond       #+#    #+#                */
+/*   Updated: 2025/07/05 17:56:33 by elhirond       ########   odam.nl        */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "sources.h"
+
+int count_visible_from_left(int *row)
+{
+    int visible;
+    int max_h;
+    int i;
+
+    visible = 0;
+    max_h = 0;
+    i = 0;
+    while (i < 4)
+    {
+        if (row[i] > max_h)
+        {
+            visible++;
+            max_h = row[i];
+        }
+        i++;
+    }
+    return visible;
+}
+
+int count_visible_from_right(int *row)
+{
+    int visible;
+    int max_h;
+    int i;
+
+    visible = 0;
+    max_h = 0;
+    i = 3;
+    while (i >= 0)
+    {
+        if (row[i] > max_h)
+        {
+            visible++;
+            max_h = row[i];
+        }
+        i--;
+    }
+    return visible;
+}
+
+int count_visible_from_top(int *col)
+{
+    int visible;
+    int max_h;
+    int i;
+
+    visible = 0;
+    max_h = 0;
+    i = 0;
+    while (i < 4)
+    {
+        if (col[i] > max_h)
+        {
+            visible++;
+            max_h = col[i];
+        }
+        i++;
+    }
+    return visible;
+}
+
+int count_visible_from_bottom(int *col)
+{
+    int visible;
+    int max_h;
+    int i;
+
+    visible = 0;
+    max_h = 0;
+    i = 3;
+    while (i >= 0)
+    {
+        if (col[i] > max_h)
+        {
+            visible++;
+            max_h = col[i];
+        }
+        i--;
+    }
+    return visible;
+}
+/*
+int main(void)
+{
+    int tab[4] = {1, 2, 3, 4};
+    printf("1, 2, 3, 4\n");
+    printf("left to right => %d \n", count_visible_from_left(tab));
+    printf("right to left <= %d \n", count_visible_from_right(tab));
+
+    int tab1[4] = {1, 2, 4, 3};
+    printf("1\n");
+    printf("2\n");
+    printf("4\n");
+    printf("3\n");
+    printf("top to bottom \\/ %d \n", count_visible_from_top(tab1));
+    printf("bottom to top /\\ %d \n", count_visible_from_bottom(tab1));
+}*/
