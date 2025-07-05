b3fcfa242ebcdb8068e1ff0ee94a36d5d18cf99c
Dragecko <dragecko@etik.com>
2025-07-05 17:32:02 +0200

    edit sources.h / Ajout de err_manager

--- ex00/sources.h
+++ ex00/sources.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                         ::::::::           */
-/*   sources.h                                           :+:    :+:           */
-/*                                                      +:+                   */
-/*   By: elhirond <marvin@42.fr>                       +#+                    */
-/*                                                    +#+                     */
-/*   Created: 2025/07/05 15:37:18 by elhirond       #+#    #+#                */
-/*   Updated: 2025/07/05 15:46:24 by elhirond       ########   odam.nl        */
+/*                                                        :::      ::::::::   */
+/*   sources.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mlopizzo <mlopizzo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/05 15:37:18 by elhirond          #+#    #+#             */
+/*   Updated: 2025/07/05 17:30:54 by mlopizzo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@
 # include <stdio.h>
 
 void	ft_putstr(char *str);
-void    ft_putchar(char c);
+void	ft_putchar(char c);
+void	err_manager(void);
 
 # define ERR "Error\n"
 
